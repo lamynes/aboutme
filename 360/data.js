b99315716817f53e5cef0fea57f3c3de57f0cbd0
lamynes <lamynes.pl@gmail.com>
2019-02-26 08:24:31 -0600

    360 shop view

---
+++ 360/data.js
@@ -0,0 +1,104 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360_00251",
+      "name": "360_0025[1]",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": 1.666813332989518,
+        "pitch": 0.150753196178524,
+        "fov": 1.2589155975593607
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-360_00231",
+      "name": "360_0023[1]",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": 1.898744133781861,
+        "pitch": -0.03080923166745997,
+        "fov": 1.2589155975593607
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-360_00141",
+      "name": "360_0014[1]",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": -1.2106624897413205,
+        "pitch": -0.1368450902460019,
+        "fov": 1.2589155975593607
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
